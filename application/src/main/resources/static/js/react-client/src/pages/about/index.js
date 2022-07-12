0c5c6864b2ba9727b4a3900e406162039d0c0ba8
zimmchad <zimmermanchad@ymail.com>
2022-07-12 16:05:18 -0400

    chadaboutpage2

--- application/src/main/resources/static/js/react-client/src/pages/about/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/about/index.js
@@ -5,31 +5,37 @@ const AboutScreen = () => {
     return (
     
         <div>
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEOcd8Of0HR6A/profile-displayphoto-shrink_200_200/0/1650207672594?e=1663200000&v=beta&t=hPZrEEgpWwJFE5xtqLYDB4oWsngmDx-486EkNOW2Dvo" alt="chadpic"></img>
             <h2>Chad Zimmerman</h2>
             <a href="https://github.com/zimmchad">Github</a>
             <br></br>
             <a href="https://www.linkedin.com/in/chad-zimmerman-4b4b35220/">LinkedIn</a>   
             <br></br>
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEFHD5EO5JleQ/profile-displayphoto-shrink_200_200/0/1654033451634?e=1663200000&v=beta&t=1LOl0lSDyfysHcCvSg6IahBDwYDGTJ3k6rCbZYBQHjw" alt="kobbypic"></img>
             <h2>Kobby Nkansah</h2>
             <a href="https://github.com/kobmank">Github</a>
             <br></br>
             <a href="https://www.linkedin.com/in/kobby-nkansah-692280218/">LinkedIn</a>
             <br></br>
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQGkPSH6TY4kpw/profile-displayphoto-shrink_200_200/0/1651268817526?e=1663200000&v=beta&t=0tFxMxekMA6_5RIZs45y2xAsk4T3KPtS4e0KYxlGqoQ" alt="noellepic"></img>
             <h2>Noelle Gisewhite</h2>
             <a href="https://github.com/noellegisewhite">Github</a>
             <br></br>
             <a href="https://www.linkedin.com/in/noellegisewhite/">LinkedIn</a>
             <br></br>
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQGog7mQ9N3D-Q/profile-displayphoto-shrink_200_200/0/1655995697589?e=1663200000&v=beta&t=6MAoLkQGEs7UIwJphXjgHP7azzWWwga4uRWtN8xuPQI" alt="zibadpic"></img>
             <h2>Zibad Maldaye</h2>
             <a href="https://www.linkedin.com/in/zibad/">LinkedIn</a>
             <br></br>
             <a>Github</a>
             <br></br>
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEOL17F7WcdHQ/profile-displayphoto-shrink_200_200/0/1648218389063?e=1663200000&v=beta&t=tErh_GGAq1KW0Wy3w6mOao5CeauDSt9OCD2wvcK6IVE" alt="pennypic"></img>
             <h2>Penny Schuenke</h2>
             <a href="https://www.linkedin.com/in/penny-schuenke/">LinkedIn</a>
             <br></br>
             <a>Github</a>
             <br></br>
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFawjkWRngADA/profile-displayphoto-shrink_200_200/0/1630002139104?e=1663200000&v=beta&t=LWSoRg6evgLsFHNlYa-zi859zxsGJZoJ_s0Jxnz2DWw" alt="hakanpic"></img>
             <h2>Hakan Celik</h2>
             <a href="https://www.linkedin.com/in/hakan-celik-84818347/">LinkedIn</a>
             <br></br>
