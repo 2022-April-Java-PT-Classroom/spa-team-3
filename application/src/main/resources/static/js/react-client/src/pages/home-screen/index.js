3bb62cfd3042f54e39bb4f1913cc1060d0cf5b4c
pennyschuenke <pennyschuenke@gmail.com>
2022-07-12 17:17:42 -0400

    updatescss/homepage

--- application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
@@ -1,8 +1,20 @@
 import React from "react";
-import style from "./style.module.scss";
+import style from './style.module.scss';
 
 const HomeScreen =()=>{
-    return("");
+    return(<div>
+        <div className="nasa">
+       <button type="onclick" id="space">Nasa</button>
+       <p>Let's explore all the planets and space with Nasa</p>
+       </div>
+
+       
+       <div className="arts">
+         <button type="onclick" id="art">Museum Of Art</button>
+         <p> Let's explore the Museum</p>
+      </div>
+    </div>
+    );
 
 }
 export default HomeScreen;
