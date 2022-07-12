1fe41e7d6d4537f7d6882c4d99c5b44d7debac08
zimmchad <zimmermanchad@ymail.com>
2022-07-12 15:49:49 -0400

    chadaboutpageinitial

--- application/src/main/resources/static/js/react-client/src/components/app-router/index.js
+++ application/src/main/resources/static/js/react-client/src/components/app-router/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Route, Switch} from 'react-router-dom';
+import AboutScreen from "../../pages/about";
 import ContactScreen from "../../pages/contact-screen";
 import HomeScreen from "../../pages/home-screen";
 
@@ -9,6 +10,7 @@ const AppRouter = () => {
         <Switch>
             <Route exact path={'/'} component={HomeScreen} />
             <Route exact path={'/contact'} component={ContactScreen} />
+            <Route exact path={'/about'} component={AboutScreen} />
         </Switch>
         
     );
--- application/src/main/resources/static/js/react-client/src/components/header/index.js
+++ application/src/main/resources/static/js/react-client/src/components/header/index.js
@@ -14,6 +14,6 @@ const Header =()=> {
                 <NavLink to={'/about'}>About</NavLink>
             </ul>
         </div>
-    )
+    );
 }
 export default Header;
