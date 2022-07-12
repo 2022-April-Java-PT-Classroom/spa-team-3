71a632bc45c09af4552f8e99927d363bf8e2ba15
noellegisewhite <noellegisewhite@gmail.com>
2022-07-12 14:01:38 -0400

    added to art page

--- application/src/main/resources/static/js/react-client/src/pages/contact-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/contact-screen/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import style from "./style.module.scss";
 
-const ContactScreen = ()=>{
-  return ("")  
+const ContactScreen = () => {
+  return (
+    <div>
+      <h2>CONTACT PAGE</h2>
+    </div>
+  );
 }
 export default ContactScreen;
