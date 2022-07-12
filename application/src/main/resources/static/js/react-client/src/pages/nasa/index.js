d72075c66390bd0cb2543c1e65918e5481c78a19
pennyschuenke <pennyschuenke@gmail.com>
2022-07-12 19:46:24 -0400

    apinasa

--- application/src/main/resources/static/js/react-client/src/pages/nasa/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/nasa/index.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { Axios } from "axios";
 import style from "./style.module.scss"
 
 const Nasa = () => {
-    return (
+    const [images,setImages] = useState(null);
+    const[loading,setLoading]=useState(true);
+
+    useEffect(() =>{
+        const fetchData = async () =>{
+            const result = await Axios("https://images-api.nasa.gov/search?q=solarsystem");
+            consolog.log(result.data);
+            setImages(result.data.collection.items);
+    };
+    if(images){
+        setLoading(false);
+    };
+    const timer = setTimeout(()=>{
+        !images && fetchData();
+
+    } ,1000);
+    return ()=> clearTimeout(timer);
+    },[images]);   
+    
+     return (
         <div>
             <h2>NASA PAGE</h2>
+            <section>
+                {loading ? <h3> Loading ...</h3> : <PlanetsImages images={images} />}
+            </section>
         </div>
     );
-}
+};
 
 export default Nasa;
