fb6710832a92eb8c0042351ebc56ebff0aef786e
zibadcoder <emushmala@yahoo.com>
2022-07-13 21:00:57 -0400

    added submit form handler function to save user input.

--- application/src/main/resources/static/js/react-client/src/pages/contact-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/contact-screen/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import FormPage from "../form/form";
 import style from "./style.module.scss";
 
 const ContactScreen = ()=>{
+  const history = useHistory();
+
+  function addContacInfo(contactData){
+    history.replace('/');
+
+  }
   return (
     <div>
       <h2 className={style.header}>Contact Us</h2>
-      <FormPage />
+      <FormPage onEnteringComment={addContacInfo}/>
     </div>
   )  
 }
--- application/src/main/resources/static/js/react-client/src/pages/form/form.js
+++ application/src/main/resources/static/js/react-client/src/pages/form/form.js
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
 import style from  './style.module.scss';
 
-const FormPage = () => {
+const FormPage = (props) => {
+
+const firstNameInput = useRef();
+const lastNameInput =useRef();
+const emailInput = useRef();
+const phoneInput =useRef();
+const commentInput = useRef();
+
+function submitController(event) {
+    event.preventDefault();
+
+    const enteredFirstName = firstNameInput.current.value;
+    const enteredLastName = lastNameInput.current.value;
+    const enteredemail = emailInput.current.value;
+    const enteredPhone = phoneInput.current.value;
+    const entereComment = commentInput.current.value;
+
+    const contactData = {
+        firstName: enteredFirstName,
+        lastName: enteredFirstName,
+        email: enteredemail,
+        phone: enteredPhone,
+        comment: entereComment,
+    }
+    console.log(contactData);
+    props.onEnteringComment(contactData);
+}
+
+
     return (
         <div>
-         <form className={style.form}>
+         <form className={style.form} onSubmit={submitController}>
           <div className={style.control}>
            <label htmlFor='first-name'> First Name</label>
-           <input type='text' required id='first-name' />
+           <input type='text' required id='first-name' ref={firstNameInput}/>
           </div>
           <div className={style.control}>
            <label htmlFor='last-name'>Last Name </label>
-           <input type='text' required id='last-name'/>
+           <input type='text' required id='last-name' ref={lastNameInput}/>
           </div>
           <div className={style.control}>
            <lable htmlFor='email'>Email</lable>
-           <input type='email' required id='email' />
+           <input type='email' required id='email' ref={emailInput} />
           </div>
           <div className={style.control}>
            <lable htmlFor='phone'>Phone Number</lable>
-           <input type='tel' required id='phone' />
+           <input type='tel' required id='phone' ref={phoneInput}/>
           </div >
           <div className={style.control}>
             <label htmlFor='description'>Comments</label>
-            <textarea id='description' required row='5'></textarea>
+            <textarea id='description' required row='5' ref={commentInput}></textarea>
           </div>
           <div className={style.actions}>
               <button>submit</button>
