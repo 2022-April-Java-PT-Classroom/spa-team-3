719ef315d1b36acc1b1919e42452f9344290550b
pennyschuenke <pennyschuenke@gmail.com>
2022-07-20 14:40:35 -0400

    animation

--- application/src/main/resources/static/js/react-client/src/components/solar-images/index.js
+++ application/src/main/resources/static/js/react-client/src/components/solar-images/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./style.module.scss";
 
 const SolarImages=({ images})=>{
     return images.map((image,index)=>{
@@ -7,9 +8,9 @@ const SolarImages=({ images})=>{
         return (
             <div key={index}>
                 <div >
-                <img src={image.links[0].href} alt=""/>
+                <img className={style.solarimg}src={image.links[0].href} alt=""/>
                 </div>
-                <div >
+                <div className={style.solarh4}>
                 <p >Title:{image.data[0].title  }</p> 
                 <h4 >Location: {image.data[0].location }</h4>
                 <h4 >Date : {image.data[0].date_created }</h4>
